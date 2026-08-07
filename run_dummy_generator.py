c5870ac590d01f9ecaf351110a7422d910af374f
Andrii Terliuk <andrey.terlyuk@gmail.com>
2026-08-07 12:13:18 +0200

    Added configs and updated the scripts

--- run_dummy_generator.py
+++ run_dummy_generator.py
@@ -3,11 +3,43 @@
 if __name__ == "__main__":
     from file_generator import FileWriterConfig, PatientConfig, DummyFileGenerator
     import logging 
-
-    patient_config = PatientConfig(id=12, surname="Doe", name="John", birth_date="1964-12-31")
-    file_writer_config = FileWriterConfig(
-              output_dir="/home/terliuk/interview_task/snkeAdvanced/data_files/", 
-              chunk_size=10000, semaphore_ext=".sem", write_interval=10.0)
+    import argparse
+    import json 
+    import os
+    from pathlib import Path
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filegenconfig", type=str, dest = "FILEGENCONFIG", required=True,
+                        help="path to the JSON file generator configuration file")
+    parser.add_argument("-p", "--patientconfig", type=str, dest = "PATIENTCONFIG", required=True,
+                        help="path to the JSON patient configuration file")
+    args = parser.parse_args()
+    ## ToDo - probably move json handlers soemwhere to make it more reusable
+    patient_config_path = args.PATIENTCONFIG
+    if patient_config_path is None or not os.path.isfile(patient_config_path):
+        raise ValueError(f"Invalid patient configuration file path: {patient_config_path}")
+    patient_config = None
+    with open(patient_config_path, "r") as f:
+        patient_config_dict = json.load(f)
+        patient_config = PatientConfig(
+            id = patient_config_dict.get("id"),
+            surname = patient_config_dict.get("surname"),
+            name = patient_config_dict.get("name"),
+            birth_date = patient_config_dict.get("birth_date")
+        )
+    ## 
+    filegen_config_path = args.FILEGENCONFIG
+    if filegen_config_path is None or not os.path.isfile(filegen_config_path):
+        raise ValueError(f"Invalid file generator configuration file path: {filegen_config_path}")
+    filegen_config = None
+    with open(filegen_config_path, "r") as f:
+        filegen_config_dict = json.load(f)
+        file_writer_config = FileWriterConfig(
+            output_dir = Path(filegen_config_dict.get("output_dir")),
+            chunk_size = filegen_config_dict.get("chunk_size"),
+            semaphore_ext = filegen_config_dict.get("semaphore"),
+            write_interval = filegen_config_dict.get("write_interval")
+        )
 
     writer = DummyFileGenerator(file_writer_config=file_writer_config, 
                                 patient_config=patient_config,
--- run_upload_app.py
+++ run_upload_app.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
+if __name__ == "__main__":
+    from uploader import UploaderApp, UploadConfig
+    import os
+    import logging
+    import json 
+    import argparse
+    from pathlib import Path
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, dest = "CONFIG", required=True,
+                        help="path to the JSON configuration file")
+    args = parser.parse_args()
 
-from uploader import UploaderApp, UploadConfig
-import os
-import logging
-import json 
-import argparse
-from pathlib import Path
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", type=str, dest = "CONFIG", required=True,
-                    help="path to the JSON configuration file")
-args = parser.parse_args()
+    config_path = args.CONFIG
+    if config_path is None or not os.path.isfile(config_path):
+        raise ValueError(f"Invalid configuration file path: {config_path}")
 
-config_path = args.CONFIG
-if config_path is None or not os.path.isfile(config_path):
-    raise ValueError(f"Invalid configuration file path: {config_path}")
+    uploader_json_dict = json.load(open(config_path, "r"))
+    uploader_config = UploadConfig(source_dir=Path(uploader_json_dict["source_dir"]),
+                                  trash_dir=Path(uploader_json_dict["trash_dir"]),
+                                  destination_url=uploader_json_dict["destination_url"],
+                                  semaphore_ext=uploader_json_dict.get("semaphore_ext", None))
 
-uploader_json_dict = json.load(open(config_path, "r"))
-uploader_config = UploadConfig(source_dir=Path(uploader_json_dict["source_dir"]),
-                               trash_dir=Path(uploader_json_dict["trash_dir"]),
-                               destination_url=uploader_json_dict["destination_url"],
-                               semaphore_ext=uploader_json_dict.get("semaphore_ext", None))
+    print(f"Uploader configuration: {uploader_config}")
+    __user = uploader_json_dict.get("username", None)
+    __password = uploader_json_dict.get("password", None)
+    if __user is None or __password is None:
+        raise ValueError("Username and password must be provided in the configuration file.")
 
-print(f"Uploader configuration: {uploader_config}")
-__user = uploader_json_dict.get("username", None)
-__password = uploader_json_dict.get("password", None)
-if __user is None or __password is None:
-    raise ValueError("Username and password must be provided in the configuration file.")
-
-uploader = UploaderApp(config=uploader_config, 
-                        user=__user, 
-                        password=__password,
-                        logfile = "uploader.log",
-                        printlevel=logging.INFO,
-                        loglevel=logging.INFO)
+    uploader = UploaderApp(config=uploader_config, 
+                            user=__user, 
+                            password=__password,
+                            logfile = "uploader.log",
+                            printlevel=logging.INFO,
+                            loglevel=logging.INFO)
